c00a5c7763eb32a6828ccb59c2bbd64e2bc02801
Fríðmundur Nattestad <fnattestad@gmail.com>
2023-04-19 12:06:23 +0000

    fixed header

--- src/components/header.js
+++ src/components/header.js
@@ -4,14 +4,19 @@ import Link from "next/link"
 export default function Header() {
     return(
         <>
-            <header className="h-16 bg-slate-800 flex items-center justify-center">
-            <Image
-                src="../../public/dumbbell.svg"
-                alt="dumbbell logo"
-                width={40}
-                height={40}
-            />
-            <Link href="/"><h1 className="text-white">GainsKeeper</h1></Link>
+            <header className="h-24 bg-slate-800 flex items-center justify-center">
+                <Link href="/">
+                    <div className="flex flex-row">
+                        <Image
+                        className="mr-2"
+                        src="/dumbbell.svg"
+                        alt="dumbbell logo"
+                        width={30}
+                        height={30}
+                        />
+                        <h1 className="text-white text-xl">GainsKeeper</h1>
+                    </div>
+                </Link>
             </header>
         </>
     )
