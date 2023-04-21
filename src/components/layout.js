11ed7c8434f6b6ca722c764bf3c1165e4a785abd
Fríðmundur Nattestad <fnattestad@gmail.com>
2023-04-21 12:49:18 +0000

    updated reps

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import Footer from "./footer";
 
 export default function Layout({children}) {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen h-fit">
             <Header/>
             <main className="flex-grow bg-[url('../../public/gym.jpg')] bg-no-repeat bg-top bg-black">{children}</main>
             <Footer/>
